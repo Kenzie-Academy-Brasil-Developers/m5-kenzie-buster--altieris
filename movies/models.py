23a9135705556456c5132e3a485b59a460f63e0e
unknown <altmsf15@gmail.com>
2023-02-23 13:45:59 -0300

    alteração no campo price da model MovieOrder

--- movies/models.py
+++ movies/models.py
@@ -19,7 +19,7 @@ class Movie(models.Model):
     
 class MovieOrder(models.Model):
     
-    price = models.FloatField(max_length=8)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     buyed_at = models.DateTimeField(auto_now_add=True)
     
     movie = models.ForeignKey("movies.Movie", on_delete=models.CASCADE, related_name='movie_order')
